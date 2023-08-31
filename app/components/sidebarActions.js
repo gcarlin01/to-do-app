feeecacc81b3a5cddf1e8e74d29183e40d4f1245
gcarlin01 <giancarlo.carlin01@gmail.com>
2023-08-31 07:44:04 -0400

    layout styling and components creation
    Co-authored-by: Luis Carlin <luiscarlin@gmail.com>

---
+++ app/components/sidebarActions.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+import sidebarActionsStyles from './sidebarActions.module.css'
+import footerButtonStyles from './footerbutton.module.css'
+
+export default function SidebarActions() {
+  return (
+    <aside className={sidebarActionsStyles.sidebarActions}>
+      <button className={footerButtonStyles.footerButton}> + New List </button>
+    </aside>
+  )
+}
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,29 @@
-import { Inter } from "next/font/google";
-import Sidebar from "./components/sidebar";
+import { Inter } from 'next/font/google'
+import Sidebar from './components/sidebar'
+import styles from './layout.module.css'
+import './globals.css'
+import SidebarActions from './components/sidebarActions'
+import MainContainer from './components/mainContainer'
+import MainActions from './components/mainActions'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "To-Do App",
-  description: "Task manager",
-};
+  title: 'To-Do App',
+  description: 'Task manager',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Sidebar />
-        <main>{children}</main>
+        <div className={styles.container}>
+          <Sidebar />
+          <MainContainer />
+          <SidebarActions />
+          <MainActions />
+        </div>
       </body>
     </html>
-  );
+  )
 }
